faffa99d23ef9db57857fec5510da565f08d8cbd
Duonius1 <mindaugas.ramonas9@gmail.com>
2024-12-07 07:14:49 +0200

    Put the recurring if/else/print statements into functions, shortened the code significantly.

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,9 +1,9 @@
 from time import sleep
 import random
 
-#TODO - Fix the recurring functions for the dealer's hidden card and the player's hit/stand phase. (Partially fixed)
 #TODO - Add a way to keep track of wins/losses.
 #TODO - Add a way to keep track of the player's money / create bet system.
+#TODO - Add splits and double downs.
 #TODO - Mayhaps a GUI?
 
 logo = r"""
@@ -17,17 +17,41 @@ logo = r"""
       `------'                           |__/           
 """
 
-def Card_Pull_Message(Card, person, stage, end): # Stage is show/pulled cards, person is for dealer/player
-    if Card == 11:
+def Card_Pull_Message(ace, person, stage, end): # Stage is show/pulled cards, person is for dealer/player
+    temp = random.choice(Cards)
+    if temp == 11:
         print(f"{person} {stage} an Ace{end}", end = "")
-    elif Card == 10:
+        ace += 1
+    elif temp == 10:
         print(f"{person} {stage} a {random.choice(Card_tens)}{end}", end = "")
     else:
-        if Card == 8:
-            print(f"{person} {stage} an {Card}{end}", end = "")
+        if temp == 8:
+            print(f"{person} {stage} an {temp}{end}", end = "")
         else:
-            print(f"{person} {stage} a {Card}{end}", end = "")
+            print(f"{person} {stage} a {temp}{end}", end = "")
+    return temp, ace
 
+def Bust_Check(hand, ace, temp, hit_stand_phase, person, person2, end):
+        if hand + temp > 21 and ace > 0:
+            hand = hand - 10 + temp
+            ace -= 1
+        elif hand + temp > 21 and temp == 11:
+            hand += 1
+            ace -= 1
+        elif hand + temp == 21:
+            hand += temp
+            print(f"{person} got a blackjack! ({hand})")
+            print("")  # Gap between next hit/stand phase
+            hit_stand_phase = False
+        elif hand + temp > 21:
+            hand += temp  # Adding to check later if Player_Hand < 21 so we skip dealer Hit/Stand phase if not needed
+            print(f"{person} busted with {hand}! {person2} {end}", end = "")
+            hit_stand_phase = False
+        else:
+            hand += temp
+        if end == "win!" and hand < 22:
+            print(f", now has {hand}.")
+        return hand, ace, hit_stand_phase
 #Important parameters for the game
 GameRunning = True
 round_index = int(1)
@@ -56,21 +80,14 @@ while GameRunning:
     Second_Dealer_Card = True
 
     # Dealer gets his first 2 cards with 1 hidden card stored in Dealer_Temp
-    Dealer_Temp = random.choice(Cards)
-    if Dealer_Temp == 11:
-        Dealer_Ace += 1
-    Card_Pull_Message(Dealer_Temp, "The dealer", "shows", "+ a hidden card.")
+    Dealer_Temp, Dealer_Ace = Card_Pull_Message(Dealer_Ace, "The dealer", "shows", " + a hidden card.\n")
     Dealer_Hand += Dealer_Temp
-    Dealer_Temp = random.choice(Cards) # Hidden card
     print("")
     sleep(1)
 
     # Give player's initial 2 cards.
     for i in range (0,2):
-        Player_Temp = random.choice(Cards)
-        Card_Pull_Message(Player_Temp, "You", "pulled", "!")
-        if Player_Temp == 11:
-            Player_Ace += 1
+        Player_Temp, Player_Ace = Card_Pull_Message(Player_Ace, "You", "pulled", "!\n")
         Player_Hand += int(Player_Temp)
         sleep(1)
 
@@ -79,37 +96,15 @@ while GameRunning:
 
     while Hit_Stand_Phase:
         if Player_Hand == 21:
-            print(f"You got a blackjack! ({Player_Hand})")
-            print("")
+            print(f"You got a blackjack! ({Player_Hand})\n")
             sleep(1)
             Hit_Stand_Phase = False
         else:
             print(f"You have {Player_Hand}, type \"H\" to Hit or \"S\" to Stand, type \"Q\" if you no longer wish to play. ", end = "")
             Hit_or_Stand = input()
             if Hit_or_Stand.lower() == "h":
-                Player_Temp = random.choice(Cards)
-                Card_Pull_Message(Player_Temp, "You", "pulled", "!")
-
-                if Player_Temp == 11:
-                    Player_Ace += 1
-
-                if Player_Hand + Player_Temp > 21 and Player_Ace > 0:
-                    Player_Hand = Player_Hand - 10 + Player_Temp
-                    Player_Ace -= 1
-                elif Player_Hand + Player_Temp > 21 and Player_Temp == 11:
-                    Player_Hand += 1
-                    Player_Ace -= 1
-                elif Player_Hand + Player_Temp == 21:
-                    Player_Hand += Player_Temp
-                    print(f"You got a blackjack! ({Player_Hand})")
-                    print("") # Gap between player's stand and dealer's hit/stand phase
-                    Hit_Stand_Phase = False
-                elif Player_Hand + Player_Temp > 21:
-                    Player_Hand += Player_Temp # Adding to check later if Player_Hand < 21 so we skip dealer Hit/Stand phase if not needed
-                    print(f"You busted with {Player_Hand}! Dealer wins!")
-                    Hit_Stand_Phase = False
-                else:
-                    Player_Hand += Player_Temp
+                Player_Temp, Player_Ace = Card_Pull_Message(Player_Ace,"You", "pulled", "!\n")
+                Player_Hand, Player_Ace, Hit_Stand_Phase = Bust_Check(Player_Hand, Player_Ace, Player_Temp, Hit_Stand_Phase, "You", "The dealer", "wins!")
             elif Hit_or_Stand.lower() == "s":
                 print("") # Gap between player's stand and dealer's hit/stand phase
                 Hit_Stand_Phase = False
@@ -124,42 +119,19 @@ while GameRunning:
     if Player_Hand <= 21 and Dealer_Hand < 17: # Basically if player is alive AND there's something to deal for dealer, otherwise no reason to go on
         while Hit_Stand_Phase:
             if Dealer_Hand < 17:
-                if Second_Dealer_Card:
-                    Card_Pull_Message(Player_Temp, "The dealer", "shows", "!")
-                    Second_Dealer_Card = False  # To prevent the dealer from showing the hidden card again
-                else:
-                    Card_Pull_Message(Player_Temp, "The dealer", "pulled", "!")
-
-                if Dealer_Temp == 11:
-                    Dealer_Ace += 1
-
-                if Dealer_Hand + Dealer_Temp > 21 and Dealer_Ace > 0:
-                    Dealer_Hand = Dealer_Hand - 10 + Dealer_Temp
-                    Dealer_Ace -= 1
-                elif Dealer_Hand + Dealer_Temp > 21 and Dealer_Temp == 11:
-                    Dealer_Hand += 1
-                    Dealer_Ace -= 1
-                elif Dealer_Hand + Dealer_Temp == 21:
-                    Dealer_Hand += Dealer_Temp
-                elif Dealer_Hand + Dealer_Temp > 21:
-                    print("\nThe dealer exceeded 21, You win, congratulations!")
-                    Dealer_Hand += Dealer_Temp
-                    Hit_Stand_Phase = False
-                else:
-                    Dealer_Hand += Dealer_Temp
-
-                if Dealer_Hand < 22:
-                    print(f", now has {Dealer_Hand}")
-                Dealer_Temp = random.choice(Cards)
+                stage = "shows" if Second_Dealer_Card else "pulled"
+                Dealer_Temp, Dealer_Ace = Card_Pull_Message(Dealer_Ace, "The dealer", stage, "")
+                Second_Dealer_Card = False  # To prevent the dealer from "showing" the hidden card again
+                Dealer_Hand, Dealer_Ace, Hit_Stand_Phase = Bust_Check(Dealer_Hand, Dealer_Ace, Dealer_Temp, Hit_Stand_Phase, "The dealer", "You", "win!")
                 sleep (1)
             else:
                 if Dealer_Hand > Player_Hand:
                     if Dealer_Hand == 21:
-                        print(f"The dealer wins with a blackjack ({Dealer_Hand})! You had {Player_Hand}")
+                        print(f"The dealer wins with a blackjack ({Dealer_Hand})! You had {Player_Hand}!")
                     else:
-                        print(f"The dealer wins with a hand of {Dealer_Hand}, you had {Player_Hand}")
+                        print(f"The dealer wins with a hand of {Dealer_Hand}, you had {Player_Hand}!")
                 elif Dealer_Hand < Player_Hand:
-                    print(f"You win with a hand of {Player_Hand}, congratulations! The dealer had {Dealer_Hand}")
+                    print(f"You win with a hand of {Player_Hand}, congratulations! The dealer had {Dealer_Hand}!")
                 else:
                     print(f"It's a tie! Both you and the dealer had a hand of {Dealer_Hand}!")
                 Hit_Stand_Phase = False
